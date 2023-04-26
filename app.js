158870d2024e6ca5a0c1e1364ef629e59bb42bb8
Isai132 <31146116+Isai132@users.noreply.github.com>
2023-04-25 18:18:32 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,210 @@
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/pikachu',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img1").src = response.sprites.front_default;
+        document.getElementById("h1").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$('#')
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/charmander',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img2").src = response.sprites.front_default;
+        document.getElementById("h2").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/squirtle',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img3").src = response.sprites.front_default;
+        document.getElementById("h3").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/bulbasaur',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img4").src = response.sprites.front_default;
+        document.getElementById("h4").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/vaporeon',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img5").src = response.sprites.front_default;
+        document.getElementById("h5").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/grimer',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img6").src = response.sprites.front_default;
+        document.getElementById("h6").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/jolteon',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img7").src = response.sprites.front_default;
+        document.getElementById("h7").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/ditto',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img8").src = response.sprites.front_default;
+        document.getElementById("h8").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/mew',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img9").src = response.sprites.front_default;
+        document.getElementById("h9").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/mewtwo',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img10").src = response.sprites.front_default;
+        document.getElementById("h10").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/snorlax',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img11").src = response.sprites.front_default;
+        document.getElementById("h11").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/tauros',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img12").src = response.sprites.front_default;
+        document.getElementById("h12").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/lapras',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img13").src = response.sprites.front_default;
+        document.getElementById("h13").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/kabuto',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img14").src = response.sprites.front_default;
+        document.getElementById("h14").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/articuno',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img15").src = response.sprites.front_default;
+        document.getElementById("h15").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/magmar',
+      type: 'GET',
+      success: (response) => {
+        document.getElementById("img16").src = response.sprites.front_default;
+        document.getElementById("h16").innerHTML = response.name;
+      },
+      error: (xhr, status, error) => {
+        console.log('ERROR');
+      }
+  });
+});
---
+++ app2.js
@@ -0,0 +1,3 @@
+$("#btn1").click(() => {
+  document.getElementById("img1.2").src = 'ejemplo.png';
+});
